d0f0f9e4fe3efd4fe2b126a5fa05cb2d4bf38cc4
Master Mind <48193931+iboss21@users.noreply.github.com>
2024-10-01 22:25:49 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version "cerulean"
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+games {"rdr3"}
+lua54 "yes"
+
+description 'Random Encounter Script for RedM'
+version '1.0.0'
+
+client_script 'client.lua'
+server_script 'server.lua'
+
+lua54 'yes'
